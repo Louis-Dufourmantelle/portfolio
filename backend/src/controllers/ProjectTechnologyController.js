70098ebc52e6d64b6c5394447ce754015929e9d9
loulou0704 <louis51@hotmail.fr>
2022-07-20 20:36:06 +0200

    created ProjectTechnologyController.js

---
+++ backend/src/controllers/ProjectTechnologyController.js
@@ -0,0 +1,83 @@
+const models = require("../models");
+
+class ProjectTechnologyController {
+  static browse = (req, res) => {
+    models.project_technology
+      .findAll()
+      .then(([rows]) => {
+        res.send(rows);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static read = (req, res) => {
+    models.project_technology
+      .find(req.params.id)
+      .then(([rows]) => {
+        if (rows[0] == null) {
+          res.sendStatus(404);
+        } else {
+          res.send(rows[0]);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static edit = (req, res) => {
+    const projectTechnology = req.body;
+
+    // TODO validations (length, format...)
+
+    projectTechnology.id = parseInt(req.params.id, 10);
+
+    models.technology
+      .update(projectTechnology)
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static add = (req, res) => {
+    const projectTechnology = req.body;
+
+    // TODO validations (length, format...)
+
+    models.project
+      .insert(projectTechnology)
+      .then(([result]) => {
+        res.status(201).send({ ...projectTechnology, id: result.insertId });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static delete = (req, res) => {
+    models.technology
+      .delete(req.params.id)
+      .then(() => {
+        res.sendStatus(204);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+}
+
+module.exports = ProjectTechnologyController;
