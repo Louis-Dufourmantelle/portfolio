4b95402266ff7d379a246745943ba0420d67f90f
loulou0704 <louis51@hotmail.fr>
2022-07-20 20:10:23 +0200

    created TechnologyController.js

---
+++ backend/src/controllers/TechnologyController.js
@@ -0,0 +1,83 @@
+const models = require("../models");
+
+class TechnologyController {
+  static browse = (req, res) => {
+    models.technology
+      .findAll()
+      .then(([rows]) => {
+        res.send(rows);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static read = (req, res) => {
+    models.technology
+      .find(req.params.id)
+      .then(([rows]) => {
+        if (rows[0] == null) {
+          res.sendStatus(404);
+        } else {
+          res.send(rows[0]);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static edit = (req, res) => {
+    const technology = req.body;
+
+    // TODO validations (length, format...)
+
+    technology.id = parseInt(req.params.id, 10);
+
+    models.technology
+      .update(technology)
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static add = (req, res) => {
+    const technology = req.body;
+
+    // TODO validations (length, format...)
+
+    models.project
+      .insert(technology)
+      .then(([result]) => {
+        res.status(201).send({ ...technology, id: result.insertId });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static delete = (req, res) => {
+    models.technology
+      .delete(req.params.id)
+      .then(() => {
+        res.sendStatus(204);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+}
+
+module.exports = TechnologyController;
