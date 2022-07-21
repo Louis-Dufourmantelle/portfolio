f7c94acde3fd4188464c1edc0fe958320bcbab6b
loulou0704 <louis51@hotmail.fr>
2022-07-21 13:16:03 +0200

    imported Home.jsx in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
-import Navbar from "./components/Navbar";
-import Profil from "./pages/Profil";
-
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Profil />
+    <div>
+      <Home />
     </div>
   );
 }
