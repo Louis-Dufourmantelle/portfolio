dfb8499bab30efe33f6ebd1daa6e6dcff378b9d8
loulou0704 <louis51@hotmail.fr>
2022-07-21 13:14:35 +0200

    created a Home.jsx page and  imported the Navbar, Skills and Profile components

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import Skills from "../components/Skills";
+import Profil from "../components/Profil";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Profil />
+      <Skills />
+    </>
+  );
+}
